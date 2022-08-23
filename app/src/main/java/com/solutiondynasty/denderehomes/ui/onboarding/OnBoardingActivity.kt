1c5cd1d84be509a8b922045c3c134fc0bd3bb2b8
Ngonidzashe.Jambo <R0pafadz0@80>
2022-08-23 16:14:49 +0200

    add: onboarding button click listeners and process flow

--- app/src/main/java/com/solutiondynasty/denderehomes/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/solutiondynasty/denderehomes/ui/onboarding/OnBoardingActivity.kt
@@ -1,10 +1,14 @@
 package com.solutiondynasty.denderehomes.ui.onboarding
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import com.solutiondynasty.denderehomes.databinding.ActivityOnBoardingBinding
+import com.solutiondynasty.denderehomes.ui.auth.AuthanticationActivity
 
 
 class OnBoardingActivity : AppCompatActivity() {
@@ -17,12 +21,32 @@ class OnBoardingActivity : AppCompatActivity() {
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setUpview()
         //todo: post list of onbarding
+
+
+
+    }
+
+    private fun setUpview() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
 
+        binding.nextLbl.setOnClickListener {
+            viewPager.currentItem = viewPager.currentItem + 1
+            binding.previousLbl.visibility = View.VISIBLE
+        }
+
+        binding.previousLbl.setOnClickListener{
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+
+        binding.skipButton.setOnClickListener{
+            val intent = Intent(this, AuthanticationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
